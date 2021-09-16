88fe1d3f5b355ed69133bd9ce220f2b4423ce121
stevencheng <stevencheng@dofun.cn>
2021-09-16 10:50:12 +0800

    仅支持单个消息的入队、出队

--- uggame-service-security-restapi/src/test/java/com/dofun/uggame/service/account/HTTPSQSTest.java
+++ uggame-service-security-restapi/src/test/java/com/dofun/uggame/service/account/HTTPSQSTest.java
@@ -2,15 +2,11 @@ package com.dofun.uggame.service.account;
 
 import com.alibaba.fastjson.JSON;
 import com.dofun.uggame.common.util.HttpSQSUtil;
-import com.dofun.uggame.common.util.RC4Util;
 import com.dofun.uggame.common.util.RandomUtil;
-import com.dofun.uggame.framework.core.id.IdUtil;
 import lombok.*;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
 @Slf4j
 public class HTTPSQSTest {
@@ -20,29 +16,24 @@ public class HTTPSQSTest {
         Integer port = 1218;
         String topicName = "test20200916";
         String auth = null;
-        List<Message> messageList=new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            messageList.add(Message.builder()
-                            .id((long) RandomUtil.getInt(100000))
-                            .sex(RandomUtil.getInt(10))
-                            .name(RandomUtil.getMixed(16))
-                            .createTime(new Date())
-                    .build());
-            Thread.sleep(1000);
-            log.info("message:{}",JSON.toJSON(messageList.get(i)));
-        }
-        HttpSQSUtil.put(ip,port,auth,topicName,messageList);
-        List<Message> list=HttpSQSUtil.get(ip,port,auth,topicName,Message.class);
-        for (Message message:list){
-            log.info("message:{}",JSON.toJSON(message));
-        }
+        Message message = Message.builder()
+                .id((long) RandomUtil.getInt(100000))
+                .sex(RandomUtil.getInt(10))
+                .name(RandomUtil.getMixed(16))
+                .createTime(new Date())
+                .build();
+        Thread.sleep(1000);
+        log.info("message before:{}", JSON.toJSONString(message));
+        HttpSQSUtil.put(ip, port, auth, topicName, JSON.toJSONString(message));
+        Message message1 = HttpSQSUtil.get(ip, port, auth, topicName, Message.class);
+        log.info("message after:{}", JSON.toJSONString(message1));
     }
 
     @Builder
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
-    public static class Message{
+    public static class Message {
         private Long id;
         private String name;
         private Integer sex;
