adbbc1db8c2c08f4006d21d12ebf2a628b388e53
stevencheng <stevencheng@dofun.cn>
2021-09-22 20:23:33 +0800

    记录消息

--- uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/mq/httpsqs/QueueMessageReceiver.java
+++ uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/mq/httpsqs/QueueMessageReceiver.java
@@ -1,9 +1,11 @@
 package com.dofun.uggame.service.security.mq.httpsqs;
 
+import com.alibaba.fastjson.JSON;
 import com.dofun.uggame.common.util.HttpSQSUtil;
 import com.dofun.uggame.service.security.clientapi.pojo.request.AccountReceiveGarenaChangePasswordRequestParam;
 import com.dofun.uggame.service.security.constants.HttPSQSConstants;
 import com.dofun.uggame.service.security.service.security.AccountService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -16,6 +18,7 @@ import org.springframework.stereotype.Component;
  * Date:2021/9/16
  * Time:17:33
  */
+@Slf4j
 @Component
 @EnableScheduling
 public class QueueMessageReceiver {
@@ -35,6 +38,7 @@ public class QueueMessageReceiver {
     public void orderNeedChangeGarenaPassword() {
         AccountReceiveGarenaChangePasswordRequestParam param = HttpSQSUtil.get(ip, port, auth, HttPSQSConstants.QUEUE_DEFINE_NOTIFY_JAVA_ORDER_NEED_CHANGE_GARENA_PASSWORD, AccountReceiveGarenaChangePasswordRequestParam.class);
         if (param != null) {
+            log.info("开始处理HTTPSQS消息:{}", JSON.toJSONString(param));
             accountService.receiveGarenaChangePassword(param);
         }
     }
