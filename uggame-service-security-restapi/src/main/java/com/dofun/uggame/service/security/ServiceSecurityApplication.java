4916a19c18beffe31da353d457b5a4c6ae1f4ab4
stevencheng <stevencheng@dofun.cn>
2021-09-23 12:08:07 +0800

    fix bug

--- uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/ServiceSecurityApplication.java
+++ uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/ServiceSecurityApplication.java
@@ -2,14 +2,26 @@ package com.dofun.uggame.service.security;
 
 
 import com.dofun.uggame.framework.core.annotation.EnableUGGameApplication;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * 安全服务
  */
+@Slf4j
 @EnableUGGameApplication
 public class ServiceSecurityApplication {
     public static void main(String[] args) {
+        log.info("系统目前默认时区为：{},现在时间是：{}",TimeZone.getDefault().getID(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        //设置为越南的时区
+        TimeZone timeZone=TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
+        log.info("修改时区为：{},",timeZone.getID());
+        TimeZone.setDefault(timeZone);
+        log.info("系统目前默认时区为：{},现在时间是：{}",TimeZone.getDefault().getID(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         SpringApplication.run(ServiceSecurityApplication.class, args);
     }
 }
