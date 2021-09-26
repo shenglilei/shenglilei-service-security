776d64a67a66932902e252f84ce6489d436ec325
stevencheng <stevencheng@dofun.cn>
2021-09-26 15:32:59 +0800

    设置越南时区

--- uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/ServiceSecurityApplication.java
+++ uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/ServiceSecurityApplication.java
@@ -16,12 +16,12 @@ import java.util.TimeZone;
 @EnableUGGameApplication
 public class ServiceSecurityApplication {
     public static void main(String[] args) {
-        log.info("系统目前默认时区为：{},现在时间是：{}", TimeZone.getDefault().getID(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        log.info("系统目前默认时区为：{},现在时间是：{}",TimeZone.getDefault().getID(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         //设置为越南的时区
-        TimeZone timeZone = TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
-        log.info("修改时区为：{},", timeZone.getID());
+        TimeZone timeZone=TimeZone.getTimeZone("Asia/Ho_Chi_Minh");
+        log.info("修改时区为：{},",timeZone.getID());
         TimeZone.setDefault(timeZone);
-        log.info("系统目前默认时区为：{},现在时间是：{}", TimeZone.getDefault().getID(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+        log.info("系统目前默认时区为：{},现在时间是：{}",TimeZone.getDefault().getID(), new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
         SpringApplication.run(ServiceSecurityApplication.class, args);
     }
 }
