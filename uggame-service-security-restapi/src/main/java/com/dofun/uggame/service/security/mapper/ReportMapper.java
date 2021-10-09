0726626d8c48f422c1354bf570da076153d2c5ec
Achin <achinliu@163.com>
2021-10-09 14:55:46 +0800

    bug修复

--- uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/mapper/ReportMapper.java
+++ uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/mapper/ReportMapper.java
@@ -3,10 +3,9 @@ package com.dofun.uggame.service.security.mapper;
 import com.dofun.uggame.framework.mysql.mappers.BaseMapper;
 import com.dofun.uggame.service.security.clientapi.pojo.request.ReportRecentFacebookStartGameRequestParam;
 import com.dofun.uggame.service.security.entity.ReportEntity;
-import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
 public interface ReportMapper extends BaseMapper<ReportEntity> {
-    List<ReportEntity> queryRecentStartGameLog(@Param("param") ReportRecentFacebookStartGameRequestParam param);
+    List<ReportEntity> queryRecentStartGameLog(ReportRecentFacebookStartGameRequestParam param);
 }
