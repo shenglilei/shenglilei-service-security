91d542cb1bc55c1c364f5ccdf6a642677968bb4e
Sheehansheng <804349336@qq.com>
2021-10-11 09:49:26 +0800

    ES用生产的默认配置

--- uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/elasticsearch/NacosESArgs.java
+++ uggame-service-security-restapi/src/main/java/com/dofun/uggame/service/security/elasticsearch/NacosESArgs.java
@@ -10,10 +10,10 @@ import org.springframework.stereotype.Component;
 @Getter
 public class NacosESArgs {
 
-    @Value(value ="${elasticSearch.host:10.0.65.67}")
+    @Value(value ="${elasticSearch.host:10.0.65.69}")
     private String EsHost;
 
-    @Value(value ="${elasticSearch.port:9200}")
+    @Value(value ="${elasticSearch.port:8081}")
     private int EsPort;
 
     @Value(value ="${elasticSearch.client.connectNum:100}")
